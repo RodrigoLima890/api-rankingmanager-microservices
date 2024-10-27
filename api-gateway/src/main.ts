5259c0de10609682cf7edb6b9c60867180ff66e1
RodrigoLima890 <rodrigodaschagas1234@gmail.com>
2024-10-27 15:45:29 -0300

    feat: create interceptor timeout

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import * as momentTimezone from 'moment-timezone';
+import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
+import { LoggingInterceptor } from './interceptors/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
 
+  app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor());
   Date.prototype.toJSON = function (): any {
     return momentTimezone(this)
       .tz('America/Sao_Paulo')
